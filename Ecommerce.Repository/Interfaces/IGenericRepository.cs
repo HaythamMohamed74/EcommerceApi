88a590a260b32d240b19bf31f0ada7ef7c4cc13b
HaythamMohamed74 <95954085+HaythamMohamed74@users.noreply.github.com>
2024-10-06 15:03:08 +0300

    Generic Repository implementation

---
+++ Ecommerce.Repository/Interfaces/IGenericRepository.cs
@@ -0,0 +1,26 @@
+﻿using Ecommerce.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommerce.Repository.Interfaces
+{
+    public interface IGenericRepository<TEntity,TKey>where TEntity : BaseEntity<TKey>
+    {
+        
+        Task <TEntity> GetByIdAsync(TKey id);
+
+
+        Task <IEnumerable<TEntity>> GetAll();
+        IQueryable<TEntity> GetAllQueryable();
+
+        Task  AddEntityAsync(TEntity entity);
+
+        void UpdateEntityAsync(TEntity entity);
+        
+        void  DeleteEntityAsync(TEntity entity);
+        
+    }
+}
---
+++ Ecommerce.Repository/Repositories/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using Ecommerce.Data.Data.contexts;
+using Ecommerce.Data.Entities;
+using Ecommerce.Repository.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ecommerce.Repository.Repositories
+{
+    public class GenericRepository<TEntity, TKey> : IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+    {
+        private readonly StoreDBContext _storeDBContext;
+
+        public GenericRepository(StoreDBContext storeDBContext)
+        {
+            _storeDBContext = storeDBContext;
+        }
+        public async Task AddEntityAsync(TEntity entity)
+        {
+              await  _storeDBContext.Set<TEntity>().AddAsync(entity);
+        }
+
+        public void DeleteEntityAsync(TEntity entity)
+        {
+            _storeDBContext.Set<TEntity>().Remove(entity);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAll()
+        {
+           return await _storeDBContext.Set<TEntity>().AsNoTracking().ToListAsync();
+        }
+
+        public IQueryable<TEntity> GetAllQueryable()
+        {
+            return _storeDBContext.Set<TEntity>().AsNoTracking();
+        }
+
+        public async Task<TEntity> GetByIdAsync(TKey id)
+        {
+         return  await _storeDBContext.Set<TEntity>().FindAsync(id);
+        }
+
+        public void UpdateEntityAsync(TEntity entity)
+        {
+            _storeDBContext.Set<TEntity>().Update(entity);
+        }
+    }
+}
