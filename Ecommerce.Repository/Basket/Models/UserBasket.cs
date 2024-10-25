5381263c2e21c3d4a9411468d80dc46d22fe68a2
HaythamMohamed74 <95954085+HaythamMohamed74@users.noreply.github.com>
2024-10-25 17:18:31 +0300

    make basketService

---
+++ Ecommerce.Repository/Basket/Models/UserBasket.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Reposatrys.Basket.Models
+{
+    public class UserBasket
+    {
+        public Guid? id { get; set; }
+        public int? DlivryMethod { get; set; }
+        public decimal ShippingPrice { get; set; }
+        public List<BasketItem> basketItems { get; set; } = new List<BasketItem>();
+        public string? PaymentIntentId { get; set; }
+        public string? ClientSecret { get; set; }
+    }
+}
