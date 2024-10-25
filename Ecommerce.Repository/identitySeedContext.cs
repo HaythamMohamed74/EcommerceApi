6740d0e9c619de3d86219088f3af769f0cb3e350
HaythamMohamed74 <95954085+HaythamMohamed74@users.noreply.github.com>
2024-10-25 17:29:58 +0300

    make Token Services

---
+++ Ecommerce.Repository/identitySeedContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Store.Data.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Reposatrys
+{
+    public class identitySeedContext
+    {
+        public async void SeedUserAsync(UserManager<AppUser> userManager)
+        {
+            if (!userManager.Users.Any())
+            {
+                var user = new AppUser
+                {
+                    DisplayName = "YoussefAbdullah",
+                    Email = "youssef@gmail.com",
+                    UserName = "JOKER",
+
+
+                };
+                await userManager.CreateAsync(user, "123456");
+            }
+        }
+
+    }
+}
+
+
+
